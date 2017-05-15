86632c14129bb736c151061156394dbb962dd71f
Neal Wright <Neal@Hendeca.com>
2017-05-14 19:51:02 -0700

    Added the conversion to an array of bits to the BlockRow component.

--- src/BlockRow.jsx
+++ src/BlockRow.jsx
@@ -1,20 +1,35 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {ZeroBlock, OneBlock} from './Block.jsx'
 import style from './BlockRow.scss'
 
+const BIN_LENGTH = 8;
+
 export class BlockRow extends Component {
-  render () {
+
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    number : PropTypes.number.isRequired
+  }
+
+  convertToBinaryArr() {
     const {number} = this.props
+    const binArr = number.toString(2).split("")
+    const padLength = BIN_LENGTH - (binArr.length - 1)
+    const padding = Array(padLength).join("0").split("")
+    return padding.concat(binArr);
+  }
+
+  render () {
+    const binArr = this.convertToBinaryArr()
     return (
       <div className={style.container}>
-        <ZeroBlock />
-        <ZeroBlock />
-        <ZeroBlock />
-        <OneBlock />
-        <ZeroBlock />
-        <ZeroBlock />
-        <OneBlock />
-        <ZeroBlock />
+        {binArr.map((bit, index) =>
+          +bit ? <OneBlock key={index} /> : <ZeroBlock key={index} />
+        )}
       </div>
     )
   }
